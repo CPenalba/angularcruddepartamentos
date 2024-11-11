3458d2f5d101fde8d93dda8a355a92a50f5a48d5
CPenalba <carolinapenalbacorpa@gmail.com>
2024-11-11 23:52:51 +0100

    Dropdown departamentos

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceDepatamentos } from '../../services/service.departamentos';
+import { Departamento } from '../../models/departamento';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  public departamentos!: Array<Departamento>;
 
+  constructor(private _service: ServiceDepatamentos) {}
+
+  ngOnInit(): void {
+    this._service.getDepartamentos().subscribe((response) => {
+      this.departamentos = response;
+    });
+  }
 }
