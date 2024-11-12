7de824ec1bf4564febfb01e9b1354d5c20619f18
CPenalba <carolinapenalbacorpa@gmail.com>
2024-11-12 09:51:20 +0100

    Details acabado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { appRoutingProvider, routing } from './app.routing';
 import { provideHttpClient } from '@angular/common/http';
 import { ServiceDepatamentos } from './services/service.departamentos';
 import { CreateComponent } from './components/create/create.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, HomeComponent, CreateComponent],
+  declarations: [AppComponent, MenuComponent, HomeComponent, CreateComponent, DetailsComponent],
   imports: [BrowserModule, routing, FormsModule],
   providers: [appRoutingProvider, provideHttpClient(), ServiceDepatamentos],
   bootstrap: [AppComponent],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,12 @@ import { ModuleWithProviders } from '@angular/core';
 
 import { HomeComponent } from './components/home/home.component';
 import { CreateComponent } from './components/create/create.component';
+import { DetailsComponent } from './components/details/details.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'create', component: CreateComponent },
+  { path: 'details/:id/:nombre/:localidad', component: DetailsComponent },
 ];
 
 export const appRoutingProvider: any[] = [];
